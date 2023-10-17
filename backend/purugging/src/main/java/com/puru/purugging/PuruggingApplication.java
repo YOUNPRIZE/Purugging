2df61994259a53a9c04a45dd8657f0f3e07e0a68
하정호 <ggk14@naver.com>
2023-10-17 17:18:09 +0900

    [Docs] Merge develop into master

---
+++ backend/purugging/src/main/java/com/puru/purugging/PuruggingApplication.java
@@ -0,0 +1,13 @@
+package com.puru.purugging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuruggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuruggingApplication.class, args);
+    }
+
+}
